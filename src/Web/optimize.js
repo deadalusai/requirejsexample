6eee55fb24f894362d5ced5d3b561c277ae45915
Benjamin Fox <deadalus.ai@gmail.com>
2017-05-25 22:21:07 +1200

    Switch to JSPM. Fix optimize.js. Loading code so sloooow.

--- src/Web/optimize.js
+++ src/Web/optimize.js
@@ -1,7 +1,7 @@
 ﻿// Build script for combining and minifying generated systemjs scripts
 const path = require('path');
 const fs = require('fs');
-const Builder = require('systemjs-builder');
+const Builder = require('jspm').Builder;
 
 
 /** Given a file-relative path, returns an absolute path */
@@ -76,50 +76,35 @@ var entryPoints =
             return importpath;
         });
 
-console.log('Loading configuration');
-
-// Load in app SystemJS config
-var factory = require(rel('./wwwroot/js/_systemjs.config.js'));
-
-var config = factory.createSystemJsConfig('./wwwroot/js/generated');
-
-// HACK HACK: Re-write library paths to be relative to this file
-var libPrefix = /^\/lib\//;
-Object.keys(config.paths).forEach(function (key) {
-    var path = config.paths[key];
-    if (libPrefix.test(path)) {
-        var newPath = path.replace(libPrefix, './wwwroot/lib/');
-        console.log(`Re-mapping library path: ${key} to ${newPath}`);
-        config.paths[key] = newPath;
-    }
-});
-
 console.log('Starting optimization');
 
-var builder = new Builder(config);
+var builder = new Builder('./wwwroot/js', './wwwroot/_jspm.config.js');
 
 // Trace the entrypoints individually and then patch their names so
 // that we can load them from the web page
 Promise.all(entryPoints.map(file => builder.trace(file)))
-    .then(trees => {
-        // HACK HACK: Re-write module names to remove .js extension added by trace.
-        return trees.map(tree => {
-            var newTree = {};
-            Object.keys(tree).forEach(name => {
-                var mod = tree[name];
-                var newName = trimExtension(mod.name);
-                console.log(`Patching module name: ${name} -> ${newName}`);
-                mod.name = newName;
-                newTree[newName] = mod;
-            });
-            return newTree;
-        });
-    })
     // Collapse all modules into a single tree
     .then(trees => trees.reduce((left, right) => builder.addTrees(left, right)))
+    // HACK HACK: Re-write module names to remove .js extension added by trace.
+    .then(tree => {
+        var newTree = {};
+        Object.keys(tree).forEach(name => {
+            var mod = tree[name];
+            // Only patch packages from our build
+            if (mod.path.indexOf('jspm_packages') === 0) {
+                newTree[name] = mod;
+                return;
+            }
+            var newName = trimExtension(mod.name);
+            console.log(`Patching module name: ${name} -> ${newName}`);
+            mod.name = newName;
+            newTree[newName] = mod;
+        });
+        return newTree;
+    })
     // And bundle into a single file
     // TODO: Split into app, dependencies files?
-    .then(tree => builder.bundle(tree, './wwwroot/js/built/main.js'))
+    .then(tree => builder.bundle(tree, './wwwroot/js/_main.js'))
     .then(result => {
         console.log('Build complete.');
     })
